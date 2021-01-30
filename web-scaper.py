fa9e279e0013d058a8df78f874d0ce61d151bca7
Denis Tatar <denis.tatar8@gmail.com>
2021-01-29 20:17:59 -0500

    Added new titles to my output, looks pretty clean.

--- web-scaper.py
+++ web-scaper.py
@@ -7,6 +7,25 @@ from bs4 import BeautifulSoup
 URL = 'https://finance.yahoo.com/quote/GME?.tsrc=applewf'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
+stockNum = 1
+
+
+#### Welcome Note ####
+print("####### Welcome to Stonk Viewer #######\n")
+
+print("########### Stock " + str(stockNum) + " ###########")
+# Printing the source name
+arrayWeb = URL.split('/')
+arraySite = arrayWeb[2].split('.')
+siteName = arraySite[1] + " " + arraySite[0]
+
+
+# Saving the Ticker
+title = str(soup.find(class_='D(ib) Fz(18px)'))
+titlePt1 = title.split(">", 1)
+titlePt2 = titlePt1[1].split("<")
+finalTitle = titlePt2[0]
+
 
 # Saving the price value
 price = soup.find(class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)')
@@ -15,24 +34,22 @@ pricePt1 = priceStr.split(">", 1)
 pricePt2 = pricePt1[1].split("<")
 finalPrice = pricePt2[0]
 
-
 # Saving the after hour price value
-afterPrice = soup.find(class_='C($primaryColor) Fz(24px) Fw(b) cc_cursor')
-# print(afterPrice.prettify())
+afterPrice = str(soup.find(class_="C($primaryColor) Fz(24px) Fw(b)"))
+afterPricePt1 = afterPrice.split(">", 1)
+afterPricePt2 = afterPricePt1[1].split("<")
+finalAfterPrice = afterPricePt2[0]
 
-
-# Printing the name of the website lol
-arrayWeb = URL.split('/')
-arraySite = arrayWeb[2].split('.')
-siteName = arraySite[1] + " " + arraySite[0]
-
+# Increasing our stock num
+stockNum += 1
 
 # Output of our information
-print("This information is being printed from: " + siteName)
-#print("TICKER: " + )
-print("PRICE: " + finalPrice)
-# print("After hours: " + afterPrice)
-
+print("SOURCE: " + siteName)
+print("COMPANY + TICKER: " + finalTitle)
+print("PRICE: " + "$" + finalPrice)
+print("AFTER HRS PRICE: " + "$" + finalAfterPrice)
+ 
+# def main():
         
 
 
